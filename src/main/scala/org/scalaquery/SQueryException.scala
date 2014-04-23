d55ce75c5dfc874d37732659fbd00156b85c4021
godenji <admin@newmindit.com>
2014-04-23 10:42:29 +0200

    formatting

--- src/main/scala/org/scalaquery/SQueryException.scala
+++ src/main/scala/org/scalaquery/SQueryException.scala
@@ -1,3 +1,4 @@
 package org.scalaquery
 
-class SQueryException(msg: String, parent: Throwable = null) extends RuntimeException(msg, parent)
+class SQueryException(msg: String, parent: Throwable = null) 
+	extends RuntimeException(msg, parent)
