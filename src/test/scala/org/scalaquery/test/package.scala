07b1d55b4513ed4a36e8406d7229ee912f2ddd0b
godenji <admin@newmindit.com>
2014-05-08 20:13:59 +0200

    println helper for Any

--- src/test/scala/org/scalaquery/test/package.scala
+++ src/test/scala/org/scalaquery/test/package.scala
@@ -2,4 +2,7 @@ package org.scalaquery
 
 package object test {
 	def echo(x: String) = System.console.printf(x + "\n")
+	implicit class AnyProvider(x: Any) { 
+    def echo: Unit = System.console.printf(x + "\n")
+  }
 }
