9012cc831af84badc38db8cd10b4b2cc9af625a8
godenji <admin@newmindit.com>
2014-05-14 19:08:41 +0200

    set eclipse to use sbt compilation target (i.e sbt is boss)

---
+++ project/Properties.scala
@@ -0,0 +1,9 @@
+trait Properties {
+	val appName         = "scala-query"
+  val appVersion      = "0.10.1"
+	val scalaRelease		= "2.11.0"
+	val tmpDir 					= "/tmp/sbt" // sbt compile target
+	val tmpfs = 
+		"TARGET_TMPFS" // linked resource, expands to abs path /tmp/sbt in eclipse
+  
+}
