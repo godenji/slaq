94720bc6a8af1bb85a76c5abb6e7a8d01fc943c2
Noah Cutler <newmind@localhost.localdomain>
2014-06-14 11:59:28 +0200

    HsqlDb apparently no longer needs explicit transaction for ResultSet when autocommit mode true

--- src/main/scala/org/scalaquery/MutatingInvoker.scala
+++ src/main/scala/org/scalaquery/MutatingInvoker.scala
@@ -35,52 +35,49 @@ trait MutatingStatementInvoker[-P,R] extends StatementInvoker[P,R] with Mutating
   protected val mutateType: ResultSetType = ResultSetType.Auto
   protected val previousAfterDelete = false
 
-  def mutate(param: P, f: ResultSetMutator[R] => Unit, end: ResultSetMutator[R] => Unit)(implicit session: Session): Unit =
-    session.withTransaction {
-      /* Hsqldb forces ResultSets to be read-only in auto-commit mode, so we
-       * use an explicit transaction. It shouldn't hurt other databases. */
-      results(param, 0, defaultConcurrency = mutateConcurrency, defaultType = mutateType).fold(
-        _ => throw new SQueryException("Cannot transform an update result"),
-        pr => try {
-          val rs = pr.rs
-          var current: R = null.asInstanceOf[R]
-          val mu = new ResultSetMutator[R] {
-            def row = current
-            def row_=(value: R) {
-              pr.restart
-              updateRowValues(pr, value)
-              rs.updateRow()
-            }
+  def mutate(param: P, f: ResultSetMutator[R] => Unit, end: ResultSetMutator[R] => Unit)(implicit session: Session): Unit = {
+    results(param, 0, defaultConcurrency = mutateConcurrency, defaultType = mutateType).fold(
+      _ => throw new SQueryException("Cannot transform an update result"),
+      pr => try {
+        val rs = pr.rs
+        var current: R = null.asInstanceOf[R]
+        val mu = new ResultSetMutator[R] {
+          def row = current
+          def row_=(value: R) {
+            pr.restart
+            updateRowValues(pr, value)
+            rs.updateRow()
+          }
+          def insert(value: R) {
+            rs.moveToInsertRow()
+            pr.restart
+            updateRowValues(pr, value)
+            rs.insertRow()
+            rs.moveToCurrentRow()
+          }
+          def delete() {
+            rs.deleteRow()
+            if(previousAfterDelete) rs.previous()
+          }
+        }
+        while(pr.nextRow) {
+          current = extractValue(pr)
+          f(mu)
+        }
+        if(end ne null) {
+          end(new ResultSetMutator[R] {
+            def row = throw new SQueryException("After end of result set")
+            def row_=(value: R) = throw new SQueryException("After end of result set")
+            def delete() = throw new SQueryException("After end of result set")
             def insert(value: R) {
               rs.moveToInsertRow()
               pr.restart
               updateRowValues(pr, value)
               rs.insertRow()
-              rs.moveToCurrentRow()
             }
-            def delete() {
-              rs.deleteRow()
-              if(previousAfterDelete) rs.previous()
-            }
-          }
-          while(pr.nextRow) {
-            current = extractValue(pr)
-            f(mu)
-          }
-          if(end ne null) {
-            end(new ResultSetMutator[R] {
-              def row = throw new SQueryException("After end of result set")
-              def row_=(value: R) = throw new SQueryException("After end of result set")
-              def delete() = throw new SQueryException("After end of result set")
-              def insert(value: R) {
-                rs.moveToInsertRow()
-                pr.restart
-                updateRowValues(pr, value)
-                rs.insertRow()
-              }
-            })
-          }
-        } finally { pr.close() }
-      )
-    }
+          })
+        }
+      } finally { pr.close() }
+    )
+  }
 }
