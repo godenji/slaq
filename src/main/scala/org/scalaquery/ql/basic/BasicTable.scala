c71643dc3eb741a51ae44cc5be38de2d0b8f4339
godenji <admin@ushr.com>
2015-02-02 18:19:31 +0200

    remove unused imports, formatting

--- src/main/scala/org/scalaquery/ql/basic/BasicTable.scala
+++ src/main/scala/org/scalaquery/ql/basic/BasicTable.scala
@@ -1,28 +1,34 @@
 package org.scalaquery.ql.basic
 
 import org.scalaquery.ql._
-import org.scalaquery.SQueryException
-import org.scalaquery.session.{PositionedResult, PositionedParameters}
 import org.scalaquery.util.Node
 
 abstract class AbstractBasicTable[T](_schemaName: Option[String], _tableName: String) 
 	extends AbstractTable[T](_schemaName, _tableName) {
 
   type ProfileType <: BasicProfile
-
   val O: BasicColumnOptions = BasicColumnOptions
 
-  def column[C : TypeMapper](n: String, options: ColumnOption[C, ProfileType]*) = new NamedColumn[C](Node(this), n, options:_*)
+  def column[C : TypeMapper](n: String, options: ColumnOption[C, ProfileType]*) = 
+  	new NamedColumn[C](Node(this), n, options:_*)
 
-  def createFinderBy[P](f: (this.type => NamedColumn[P]))(implicit profile: BasicProfile, tm: TypeMapper[P]): BasicQueryTemplate[P,T] = {
+  def createFinderBy[P](f: this.type => NamedColumn[P])
+  	(implicit profile: BasicProfile, tm: TypeMapper[P]): BasicQueryTemplate[P,T] = {
+  	
     import profile.Implicit._
-    Params[P](tm).flatMap(p => Query(this).where(t => ColumnOps.Is(f(t.asInstanceOf[AbstractBasicTable.this.type]), p)))(profile)
+    Params[P](tm).flatMap{p=> 
+    	Query(this).filter{t=> 
+    		ColumnOps.Is( f(t.asInstanceOf[AbstractBasicTable.this.type] ), p)
+    	}
+    }(profile)
   }
 
   def ddl(implicit profile: ProfileType): DDL = profile.buildTableDDL(this)
 }
 
-abstract class BasicTable[T](_schemaName: Option[String], _tableName: String) extends AbstractBasicTable[T](_schemaName, _tableName) {
+abstract class BasicTable[T](_schemaName: Option[String], _tableName: String) 
+	extends AbstractBasicTable[T](_schemaName, _tableName) {
+	
   def this(_tableName: String) = this(None, _tableName)
   type ProfileType = BasicProfile
 }
