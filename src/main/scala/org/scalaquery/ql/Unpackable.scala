2d5ca27be0896fa580e1bccae78ee3248fb6beeb
godenji <admin@ushr.com>
2015-02-02 10:17:20 +0200

    formatting/cleanup

--- src/main/scala/org/scalaquery/ql/Unpackable.scala
+++ src/main/scala/org/scalaquery/ql/Unpackable.scala
@@ -7,8 +7,13 @@ import org.scalaquery.util.{Node, ValueLinearizer}
  */
 case class Unpackable[T, U](value: T, unpack: Unpack[T, U]) {
   def endoMap(f: T => T): Unpackable[T, U] = new Unpackable(f(value), unpack)
-  def reifiedNode = Node(unpack.reify(value))
-  def reifiedUnpackable[R](implicit ev: Reify[T, R]): Unpackable[R, U] = Unpackable(unpack.reify(value).asInstanceOf[R], unpack.reifiedUnpack.asInstanceOf[Unpack[R, U]])
   def linearizer = unpack.linearizer(value).asInstanceOf[ValueLinearizer[U]]
   def mapOp(f: Node => Node) = unpack.mapOp(value, f).asInstanceOf[T]
+  
+  def reifiedNode = Node(unpack.reify(value))
+  def reifiedUnpackable[R](implicit ev: Reify[T, R]): Unpackable[R, U] = 
+  	Unpackable(
+  		unpack.reify(value).asInstanceOf[R], 
+  		unpack.reifiedUnpack.asInstanceOf[Unpack[R, U]]
+  	)
 }
