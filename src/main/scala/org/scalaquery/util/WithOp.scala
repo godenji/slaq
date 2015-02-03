489e6f163c64be5667c7728a815b80e947248e7f
godenji <admin@ushr.com>
2015-02-03 10:44:12 +0200

    remove withOp.fm source generator, don't need anymore; add WithOp.scala

---
+++ src/main/scala/org/scalaquery/util/WithOp.scala
@@ -0,0 +1,16 @@
+package org.scalaquery.util
+
+trait WithOp extends Cloneable {self:Node=>
+  def mapOp(f: Node => Node): this.type = {
+    val t = clone
+    t._op = f(this)
+    t
+  }
+  private[WithOp] var _op: Node = _
+  final def op: Node = _op
+  override def clone(): this.type = super.clone.asInstanceOf[this.type]
+}
+
+object WithOp {
+  def unapply(w: WithOp) = if(w.op == null) None else Some(w.op)
+}
