9f81ee2beb8646ff1d7dc4b6fa8802e43fc3a592
godenji <admin@ushr.com>
2015-03-20 11:07:51 +0200

    upgrade to scala 2.11.6 and sbt 0.13.6

--- project/Properties.scala
+++ project/Properties.scala
@@ -1,7 +1,7 @@
 trait Properties {
 	val appName         = "scala-query"
   val appVersion      = "0.10.1"
-	val scalaRelease		= "2.11.2"
+	val scalaRelease		= "2.11.6"
 	val tmpDir 					= "/tmp/sbt" // sbt compile target
 	val tmpfs = 
 		"TARGET_TMPFS" // linked resource, expands to abs path /tmp/sbt in eclipse
