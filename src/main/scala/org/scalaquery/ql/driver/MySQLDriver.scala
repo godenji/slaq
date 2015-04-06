90f665089c81da762e99d6df7b6e6343f931712f
godenji <admin@ushr.com>
2015-04-06 09:42:02 +0200

    implement currval

--- src/main/scala/org/scalaquery/ql/driver/MySQLDriver.scala
+++ src/main/scala/org/scalaquery/ql/driver/MySQLDriver.scala
@@ -118,25 +118,42 @@ class MySQLSequenceDDLBuilder[T](seq: Sequence[T], profile: MySQLDriver) extends
     val start = seq._start.getOrElse(if(desc) maxValue else minValue)
     val beforeStart = start - increment
     if(!seq._cycle && (seq._minValue.isDefined && desc || seq._maxValue.isDefined && !desc))
-      throw new SQueryException("Sequences with limited size and without CYCLE are not supported by MySQLDriver's sequence emulation")
+      throw new SQueryException(
+      	"Sequences of limited size and no CYCLE are not supported by MySQLDriver's sequence emulation"
+      )
     val incExpr = if(seq._cycle) {
-      if(desc) "if(id-"+(-increment)+"<"+minValue+","+maxValue+",id-"+(-increment)+")"
-      else "if(id+"+increment+">"+maxValue+","+minValue+",id+"+increment+")"
+      if(desc) 
+      	s"if(id-${-increment}<$minValue,$maxValue,id-${-increment})"
+      else
+      	s"if(id+${increment}>$maxValue,$minValue,id+${increment})"
     } else {
       "id+("+increment+")"
     }
-    //TODO Implement currval function
     new DDL {
-      val createPhase1 = Iterable(
-        "create table " + quote(seq.name + "_seq") + " (id " + t + ")",
-        "insert into " + quote(seq.name + "_seq") + " values (" + beforeStart + ")",
-        "create function " + quote(seq.name + "_nextval") + "() returns " + sqlType + " begin update " +
-          quote(seq.name + "_seq") + " set id=last_insert_id(" + incExpr + "); return last_insert_id(); end")
+    	private val(seqName,currVal,nextVal) = (
+    		s"${seq.name}_seq", 
+    		s"${seq.name}_currval", 
+    		s"${seq.name}_nextval"
+    	)
+      val createPhase1 = Iterable(	
+        s"create table ${quote(seqName)} (id $t)",
+        s"insert into ${quote(seqName)} values ($beforeStart)",
+        s"""
+create function ${quote(nextVal)}() returns $sqlType begin update 
+${quote(seqName)} set id=last_insert_id($incExpr); return last_insert_id(); end
+""",
+        s"""
+create function ${quote(currVal)}() returns $sqlType begin 
+select max(id) into @v from ${quote(seqName)}; return @v; end
+"""
+      )
       val createPhase2 = Nil
       val dropPhase1 = Nil
       val dropPhase2 = Iterable(
-        "drop function " + quote(seq.name + "_nextval"),
-        "drop table " + quote(seq.name + "_seq"))
+      	s"drop function ${quote(currVal)}",
+        s"drop function ${quote(nextVal)}",
+        s"drop table ${quote(seqName)}"
+      )
     }
   }
 }
