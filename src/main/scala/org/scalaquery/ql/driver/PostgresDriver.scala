0cbc53ba01f5187462d93a55634be3b1dbd3d7b6
godenji <admin@ushr.com>
2015-04-16 09:36:44 +0200

    fix take/drop; implement currval/nextval; implement implicit group by clause append to satisfy ansi requirement that all tables selected in a group by query must include at least one functionally dependent column (e.g. primary key) -- this functionality enables one to select entire table rows without having to aggregate individuals columns with absurd hacks like u.firstName.min (i.e. MIN(u.firstName))

--- src/main/scala/org/scalaquery/ql/driver/PostgresDriver.scala
+++ src/main/scala/org/scalaquery/ql/driver/PostgresDriver.scala
@@ -46,20 +46,101 @@ class PostgresTypeMapperDelegates extends TypeMapperDelegates {
 class PostgresQueryBuilder(_query: Query[_,_], _nc: NamingContext, parent: Option[QueryBuilder], profile: PostgresDriver)
 extends QueryBuilder(_query, _nc, parent, profile) {
 
-
   override type Self = PostgresQueryBuilder
   override protected val concatOperator = Some("||")
 
   protected def createSubQueryBuilder(query: Query[_,_], nc: NamingContext) =
     new PostgresQueryBuilder(query, nc, Some(this), profile)
+  
+  override protected def innerExpr(c: Node, b: SQLBuilder): Unit = c match {
+    case Sequence.Nextval(seq) => b += s"nextval('${seq.name}')"
+    case Sequence.Currval(seq) => b += s"currval('${seq.name}')"
+    case _ => super.innerExpr(c, b)
+  }
 
   override protected def appendLimitClause(b: SQLBuilder) = query.typedModifiers[TakeDrop].lastOption.foreach {
-    case TakeDrop(Some(t), Some(d), compareNode) => 
-    	appendLimitValue(b+=" LIMIT ", t, compareNode); appendLimitValue(b+=" OFFSET ",d)
-    	
-    case TakeDrop(Some(t), None, _) => appendLimitValue(b+=" LIMIT ",t)
-    case TakeDrop(None, Some(d), _) => appendLimitValue(b+=" OFFSET ",d)
+  	case TakeDrop(Some(t), Some(d), compareNode) =>
+  		val compFn = maybeLimitNode(t,d,compareNode,_:Boolean)
+  		appendLimitValue(b+=" LIMIT ", t, compFn(false))  
+  		appendLimitValue(b+=" OFFSET ", d, compFn(true))
+  		
+    case TakeDrop(Some(t), None, _) => appendLimitValue(b+=" LIMIT ", t)
+    case TakeDrop(None, Some(d), _) => appendLimitValue(b+=" OFFSET ", d)
     case _ =>
+  }
+  
+  override protected def appendGroupClause(b: SQLBuilder): Unit = 
+  	query.typedModifiers[Grouping] match {
+    	case Nil =>
+    	case xs =>
+    		b += " GROUP BY "
+    		b.sep(xs, ",")(x=> expr(x.by, b, false, true))
+    		
+    		// existing groupBy columns
+    		val groupCols = xs.collect(_.by match{
+    			case nc: NamedColumn[_] => Some(nc.name)
+    			case _=> None
+    		}).flatten
+    		
+    		//query.reified.dump("reif")
+    		ansiAppendGroupClause(query.reified, groupCols)(b)
+  	}
+  
+  /*
+   * SQL standard requires that queries with a group by clause
+   * must include non-aggregate columns from select list 
+   * where a functional dependency exists. 
+   * 
+   * In practice, the PK column of dependent table *not* explicitly 
+   * referenced in group by clause can be implicitly appended, 
+   * thus satisfying the standard while avoiding useless boilerplate 
+   * in queries.
+   * 
+   * example sql standard breaking query:
+   * 	for{o <- Order; u <- o.user; groupBy o.userID} yield(o,u)
+   * 
+   * with implicit appending of dependent cols the following
+   * standards compliant sql is generated:
+   * 
+   * select o.*, u.* 
+   * from order o, user u 
+   * where o.userID = u.id
+   * group by o.userID, u.id // u.id implicitly appended 
+   */
+  private def ansiAppendGroupClause(
+  	node: Node, groupCols: List[String])(b: SQLBuilder): Unit = {
+  	
+  	def matchColumn(n: NamedColumn[_]) = {
+  		if( // append if is PK and not already exist in groupCols
+				n.options.exists(_ == ColumnOption.PrimaryKey) && 
+				!groupCols.exists(_ == n.name)
+			)
+  		{b += ","; expr(n,b,false,true)}
+  	}
+  	def matchTable(n: Node) = n match{
+  		case ta @ Table.Alias(t) => 
+				ansiAppendGroupClause(t,groupCols)(b)
+			case t: Table[_]=> 
+				ansiAppendGroupClause(t.*,groupCols)(b)
+  	}
+  	node match{
+	  	case p: ProductNode =>
+				p.nodeChildren.foreach{x=> x match{
+					// aggregrate column, no need to append 
+					case _:SimpleFunction | ColumnOps.CountDistinct(_) =>
+					case _:Table[_] | _:Table.Alias => matchTable(x)
+					case nc: NamedColumn[_] => matchColumn(nc)
+					case p: Projection[_] => 
+						p.nodeChildren.foreach{case(nc:NamedColumn[_])=> 
+							matchColumn(nc)
+						}
+					case n=>
+						//println(s"inner fallback $n")
+						b += ","; expr(n,b,false,true)
+				}}
+	  	case _:Table[_] | _:Table.Alias => matchTable(node)
+			case _=> //println(s"outer fallback $node")
+  	}
   }
 }
 
