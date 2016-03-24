9929435dccea9648daaa3648ab57e0ccfba939e0
godenji <admin@ushr.com>
2016-03-24 12:57:20 -0400

    fix case-when deprecations (refactored to If Then Else)

--- src/main/scala/org/scalaquery/ql/Case.scala
+++ src/main/scala/org/scalaquery/ql/Case.scala
@@ -1,34 +1,60 @@
 package org.scalaquery.ql
 
 import org.scalaquery.util.{Node, BinaryNode}
+import org.scalaquery.SQueryException
 
 object Case {
+	
+  class WhenNode(
+  	val left: Node, 
+  	val right: Node) extends BinaryNode
 
-  class WhenNode(val left: Node, val right: Node) extends BinaryNode
-
-  abstract class CaseColumn[T : TypeMapper](val clauses: List[WhenNode], val elseClause: Node) extends Column[T] {
+  abstract class CaseColumn[T : TypeMapper](
+  	val clauses: List[WhenNode], 
+  	val elseClause: Node) extends Column[T] {
+  	
     def nodeChildren = elseClause :: clauses
   }
 
-  def when[C <: Column[_] : Queryable](cond: C) = new UntypedWhen(Node(cond))
+  def If[C <: Column[_] : 
+  	Queryable](cond: C) = new UntypedWhen(Node(cond))
 
   class UntypedWhen(cond: Node) {
-    def then[B : BaseTypeMapper](res: Column[B]) = new TypedCase[B,B](new WhenNode(cond, Node(res)) :: Nil)
-    def then[B](res: Column[Option[B]]) = res.typeMapper match {
-      case tmt: OptionTypeMapper[_] =>
-        new TypedCase[B,Option[B]](new WhenNode(cond, Node(res)) :: Nil)(tmt.base, tmt)
+    def Then[B : BaseTypeMapper](res: Column[B]) = 
+    	new TypedCase[B,B](
+    		new WhenNode(cond, Node(res)) :: Nil
+    	)
+    def Then[B](res: Column[Option[B]]) = res.typeMapper match {
+      case tmt: OptionTypeMapper[_] => 
+      	new TypedCase[B,Option[B]](
+      		new WhenNode(cond, Node(res)) :: Nil
+      	)(tmt.base, tmt)
+      case x => 
+      	throw new SQueryException(s"""No "Then" match in case-when clause for $x""")
     }
   }
 
-  class TypedCase[B : TypeMapper, T : TypeMapper](clauses: List[WhenNode])
-  extends CaseColumn[Option[B]](clauses, ConstColumn.NULL) {
-    def when[C <: Column[_] : Queryable](cond: C) = new TypedWhen[B,T](cond, this)
-    def otherwise(res: Column[T]): Column[T] = new TypedCaseWithElse[T](clauses, Node(res))
+  class TypedCase[B : TypeMapper, T : TypeMapper]
+  	(clauses: List[WhenNode])
+  	extends CaseColumn[Option[B]](clauses, ConstColumn.NULL) {
+  	
+    def If[C <: Column[_] : 
+    	Queryable](cond: C) = new TypedWhen[B,T](cond, this)
+    	
+    def Else(res: Column[T]): 
+    	Column[T] = new TypedCaseWithElse[T](clauses, Node(res))
   }
 
-  class TypedWhen[B : TypeMapper, T : TypeMapper](cond: Node, parent: TypedCase[B,T]) {
-    def then(res: Column[T]) = new TypedCase[B,T](new WhenNode(cond, Node(res)) :: parent.clauses)
+  class TypedWhen[B : TypeMapper, T : TypeMapper]
+  	(cond: Node, parent: TypedCase[B,T]) {
+  	
+    def Then(res: Column[T]) = 
+    	new TypedCase[B,T](
+    		new WhenNode(cond, Node(res)) :: parent.clauses
+    	)
   }
-
-  class TypedCaseWithElse[T : TypeMapper](clauses: List[WhenNode], elseClause: Node) extends CaseColumn[T](clauses, elseClause)
+  
+  class TypedCaseWithElse[T : TypeMapper]
+  	(clauses: List[WhenNode], elseClause: Node)
+  	extends CaseColumn[T](clauses, elseClause)
 }
