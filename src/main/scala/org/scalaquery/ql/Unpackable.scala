c7631f39902c1269f74ee349b666370182aad975
godenji <admin@ushr.com>
2016-03-26 10:25:49 -0400

    removed endoMap, no longer needed

--- src/main/scala/org/scalaquery/ql/Unpackable.scala
+++ src/main/scala/org/scalaquery/ql/Unpackable.scala
@@ -6,7 +6,6 @@ import org.scalaquery.util.{Node, ValueLinearizer}
  * A packed value together with its unpacking
  */
 case class Unpackable[T, U](value: T, unpack: Unpack[T, U]) {
-  def endoMap(f: T => T): Unpackable[T, U] = new Unpackable(f(value), unpack)
   def linearizer = unpack.linearizer(value).asInstanceOf[ValueLinearizer[U]]
   def mapOp(f: Node => Node) = unpack.mapOp(value, f).asInstanceOf[T]
   
