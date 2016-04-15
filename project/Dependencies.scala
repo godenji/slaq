20c90c3dd021334093922a6c513d74165b20325c
godenji <admin@ushr.com>
2016-04-15 10:48:38 +0200

    update driver deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import Keys._
 
 trait Dependencies{
 	val appDeps = Seq(
-	  "com.h2database" % "h2" % "1.4.185" % "test",
-	  "org.xerial" % "sqlite-jdbc" % "3.8.7" % "test",
-	  "org.hsqldb" % "hsqldb" % "2.3.2" % "test",
-	  "org.postgresql" % "postgresql" % "9.4-1201-jdbc41" % "test",
-	  "mysql" % "mysql-connector-java" % "5.1.34" % "test",
+	  "com.h2database" % "h2" % "1.4.191" % "test",
+	  "org.xerial" % "sqlite-jdbc" % "3.8.11.2" % "test",
+	  "org.hsqldb" % "hsqldb" % "2.3.3" % "test",
+	  "org.postgresql" % "postgresql" % "9.4.1208.jre7" % "test",
+	  "mysql" % "mysql-connector-java" % "5.1.38" % "test",
 	  "net.sourceforge.jtds" % "jtds" % "1.3.1" % "test",
 	  "com.novocode" % "junit-interface" % "0.11" % "test"
 	)
