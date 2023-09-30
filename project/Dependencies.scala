75695540e319a4927b5ee0229b40b9a7ed08bdbc
godenji <admin@ushr.com>
2023-09-30 10:28:48 -0700

    upgrade mysql driver version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ trait Dependencies {
     "org.xerial" % "sqlite-jdbc" % "3.36.0.3" % "test",
     "org.hsqldb" % "hsqldb" % "2.6.1" % "test",
     "org.postgresql" % "postgresql" % "42.3.1" % "test",
-    "mysql" % "mysql-connector-java" % "8.0.27" % "test",
+    "mysql" % "mysql-connector-java" % "8.0.33" % "test",
     "net.sourceforge.jtds" % "jtds" % "1.3.1" % "test",
     "com.novocode" % "junit-interface" % "0.11" % "test"
   )
