2eede53ee76ff6d1a540a06e3f999970e89a5f17
godenji <admin@ushr.com>
2024-06-27 08:46:04 -0700

    prefix schema name (if exist) to table name in delete statements

--- src/main/scala/slaq/scalaquery/ql/core/DeleteBuilder.scala
+++ src/main/scala/slaq/scalaquery/ql/core/DeleteBuilder.scala
@@ -8,15 +8,21 @@ trait DeleteBuilder { self: QueryBuilder & QueryBuilderAction =>
   import profile.sqlUtils._
 
   object Delete {
+    private def prefixSchema(t: Table[?]): String = {
+      t.schemaName
+        .map(schema => s"${quote(schema)}.")
+        .getOrElse("")
+    }
+
     def build: SqlBuilder.Result = {
       val b = new SqlBuilder += "DELETE FROM "
-      val (table, tableName) = query.reified match {
-        case ta @ Table.Alias(t: Table[_]) => (ta, t.tableName)
-        case t: Table[_]                   => (t, t.tableName)
+      val (table, tableName, schemaName) = query.reified match {
+        case ta @ Table.Alias(t: Table[_]) => (ta, t.tableName, prefixSchema(t))
+        case t: Table[_]                   => (t, t.tableName, prefixSchema(t))
         case n =>
           Fail(s"Cannot create DELETE statement with $n; table required")
       }
-      b += quote(tableName)
+      b += s"${schemaName}${quote(tableName)}"
       overrideAlias(table, tableName) // delete statement does not support aliasing
       appendConditions(b)
       if (tableAliases.size > 1) Fail(
